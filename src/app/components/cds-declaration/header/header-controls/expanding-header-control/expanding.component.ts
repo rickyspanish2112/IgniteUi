d763691b2de6a617128614af75f8383a0089892a
Ross Starkey <ross.starkey@asm.org.uk>
2019-10-27 07:47:06 +0000

    add logic to change expanding panel title

--- src/app/components/cds-declaration/header/header-controls/expanding-header-control/expanding.component.ts
+++ src/app/components/cds-declaration/header/header-controls/expanding-header-control/expanding.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IgxExpansionPanelComponent } from 'igniteui-angular';
 
 @Component({
   selector: 'app-expanding',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expanding.component.scss']
 })
 export class ExpandingComponent implements OnInit {
+  @ViewChild(IgxExpansionPanelComponent, { read: IgxExpansionPanelComponent, static: true })
+  public panel: IgxExpansionPanelComponent;
+
+  public ExpandedDescription = 'Expand to display fields relating to a CDS Declaration header.';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public handleExpansion(evt?: { event: Event }) {
+    this.ExpandedDescription = 'Collaspe to hide fields relating to a CDS Declaration header.';
+  }
+
+  public handleCollapse(evt?: { event: Event }) {
+    this.ExpandedDescription = 'Expand to display fields relating to a CDS Declaration header.';
   }
 
 }
